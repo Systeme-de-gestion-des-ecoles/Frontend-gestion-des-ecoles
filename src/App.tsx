44e0d64120e07409898a5ec16b5303447d90cc01
Spinoza235 <esperancedingammadji785@gmail.com>
2025-05-12 20:44:16 +0200

    Remplace l'ancien projet par le nouveau

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl bg- font-bold underline">
+      Hello world!
+
+      <p className="bg-blue-600">Hi  rrainna </p>
+    </h1>
+  )
+}
