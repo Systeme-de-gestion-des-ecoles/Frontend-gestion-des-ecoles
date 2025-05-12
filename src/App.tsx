486fd49f5286aba8c9f5b9cc6db5b788ce47ccb4
Spinoza235 <esperancedingammadji785@gmail.com>
2025-05-12 20:55:37 +0200

    suppression

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ export default function App() {
   return (
     <h1 className="text-3xl bg- font-bold underline">
       Hello world!
-
-      <p className="bg-blue-600">Hi  rrainna </p>
     </h1>
   )
 }
